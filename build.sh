c6e2b040fbc92155342869609676c5b1d299f9b7
ximother <gotoximm@gmail.com>
2022-11-19 22:54:46 +0800

    chore: build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ BUILDPATH=./docs
 mkdir $BUILDPATH
 
 echo "Copying files..."
-cp -RX ./src/ $BUILDPATH/
+cp -R ./src/ $BUILDPATH/
 
 for file in $( find src -type f -name "*.html"); do
   html-minifier $file --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js > $BUILDPATH/${file#*src/} &&
